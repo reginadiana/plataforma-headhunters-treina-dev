2327ec6c662f606326224ac01317b06c9878b097
Diana-ops <dianaregina22@outlook.com.br>
2020-05-14 23:51:00 -0300

    Adicionando validacoes ao model de Vaga e nivel

--- app/models/job_opportunity.rb
+++ app/models/job_opportunity.rb
@@ -1,4 +1,17 @@
 class JobOpportunity < ApplicationRecord
 	belongs_to :level
 	belongs_to :headhunter
+
+	validates :title, :company, :description_job, :skills, :salary_range, :deadline, :office_functions, presence: true
+	validates :title, uniqueness: true
+	validates :salary_range , :numericality => { greater_than: 1045}
+	validates :deadline, :numericality => { greater_than: Date.today }
+
+	validate :deadline_date_cannot_be_in_the_past
+
+	def deadline_date_cannot_be_in_the_past
+		if deadline_date.present? && deadline_date < Date.today
+			errors.add(:deadline, "não pode estar no passado")
+		end
+	end
 end
