c8a3932865dbbae6237da6537652891c89a1d6a6
Diana-ops <dianaregina22@outlook.com.br>
2020-05-15 19:09:43 -0300

    Escrevendo TDD para visitantes nao acessarem rotas de job_oportunities

---
+++ spec/features/job_opportunities/candidate_authorization_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'Candidate can not' do
+	before :each do
+		@job_opportunity = create(:job_opportunity, title: 'Cientista de Dados')
+		user = create(:user)
+		login_as user, scope: :user
+	end
+	
+	scenario 'to create some job by route' do
+		visit new_job_opportunity_path
+		expect(current_path).to eq(job_opportunities_path)
+	end
+
+	scenario 'to edit some job by route' do
+		visit edit_job_opportunity_path(@job_opportunity)
+		expect(current_path).to eq(job_opportunities_path)
+	end
+
+	scenario 'register valid job' do
+
+		visit root_path
+
+		expect(page).to have_content('Cientista de Dados')
+		expect(page).not_to have_link 'Cadastrar nova Vaga'
+	end
+
+	scenario 'edit job' do
+
+		visit root_path
+		click_on 'Cientista de Dados'
+
+		expect(page).not_to have_link 'Editar'
+	end
+	scenario 'delete job' do
+
+		visit root_path
+		click_on 'Cientista de Dados'
+
+		expect(page).not_to have_link 'Excluir'
+	end
+end
+
