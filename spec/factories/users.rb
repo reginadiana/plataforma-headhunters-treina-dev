b6db8d0ddc224e30fb76c1f8d91029a8b91dbd33
Diana-ops <dianaregina22@outlook.com.br>
2020-05-24 14:53:50 -0300

    Headhunter pode ver o numero de vagas, perfils e candidatos cadastrados - Candidatos podem ver o numero de vagas

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     	sequence(:email) { |n| "email#{n}@test.com.br" }
 	password { '12345678' }
+
+    trait :with_avatar do
+      avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'camila-avatar.jpg'), 'image/jpg') }
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,9 @@ end
 RSpec.configure do |config|
   config.include Warden::Test::Helpers	
   config.include FactoryBot::Syntax::Methods
+  FactoryBot::SyntaxRunner.class_eval do
+    include ActionDispatch::TestProcess
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
